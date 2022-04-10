e6417d9012221689a6777ffdb07544ea8d290841
Shaurya Sharma <79473274+ShaSha-Codes@users.noreply.github.com>
2022-04-10 15:31:09 +0530

    Almost done

--- src/App.js
+++ src/App.js
@@ -3,58 +3,62 @@ import Navbar from './components/Navbar'
 import Card from './components/Card'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import img1 from './images/1.png'
 import img2 from './images/2.png'
 import img3 from './images/3.png'
 import img4 from './images/4.jpg'
 import img5 from './images/5.jpg'
+import { nanoid } from 'nanoid'
 
 function App() {
   let data=[
     {image:img3,
     title:"Water Jug Problem",
-    description:"A green iguana is a species of lizard native to the tropical rainforests of the Americas. It is a large, colorful, and carnivorous lizard that is found in the wild in the Americas.",
-    link:"https://www.google.com"},
+    description:"Two jugs of different measurements are given these jugs do not have an intermediate measuring markings. Our goal is to procure some x litres of water in the end.",
+    link:"https://6249cefc4549ff085a3c1dfc--mellifluous-strudel-ce1f5d.netlify.app/"},
 
     {image:img1,
     title:"8 Puzzle",
-    description:"A green iguana is a species of lizard native to the tropical rainforests of the Americas. It is a large, colorful, and carnivorous lizard that is found in the wild in the Americas.",
-    link:"https://www.google.com"},
+    description:"Given a 3×3 board with 8 tiles and one empty space.The objective is to place the numbers on tiles to match the final configuration using the empty space.",
+    link:"https://ornate-cocada-437045.netlify.app/"},
 
     {image:img2,
     title:"Tower of Hanoi",
-    description:"A green iguana is a species of lizard native to the tropical rainforests of the Americas. It is a large, colorful, and carnivorous lizard that is found in the wild in the Americas.",
-    link:"https://www.google.com"},
+    description:"Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move the entire stack to another rod.",
+    link:"https://astounding-bunny-2a2f7a.netlify.app/"},
 
     {image:img4,
     title:"N Queens",
-    description:"A green iguana is a species of lizard native to the tropical rainforests of the Americas. It is a large, colorful, and carnivorous lizard that is found in the wild in the Americas.",
-    link:"https://www.google.com"},
+    description:"N queen is a classic backtracking problem in which we have to place n queens in nxn chessboard.",
+    link:"https://nqueenalgo.netlify.app/"},
 
     {image:img5,
     title:"Maze",
-    description:"A green iguana is a species of lizard native to the tropical rainforests of the Americas. It is a large, colorful, and carnivorous lizard that is found in the wild in the Americas.",
-    link:"https://www.google.com"}
+    description:"A maze-solving algorithm is an automated method for the solving a maze. ",
+    link:"https://624a452f0216d16840dba64e--thunderous-madeleine-089f81.netlify.app/"}
   ]
 
 
   function CardCreator(){
     let cards=[]
     for(let i=0;i<data.length;i++){
-      cards.push(<Card image={data[i].image} title={data[i].title} description={data[i].description} link={data[i].link}/>)
+      cards.push(<Grid item md={4}  xs={12} key={nanoid()} ><Card  image={data[i].image} title={data[i].title} description={data[i].description} link={data[i].link}/></Grid>)
     }
     return cards
   }
   return (
     <div className="App">
       <Navbar />
-      <Box mt={35} sx={{ flexGrow: 1 }}>
-        <Grid  container  alignItems="center"
-  justifyContent="center" spacing={2}>
-            {CardCreator()}
-        </Grid>
-      </Box>
-      
+      <Stack alignItems="center">
+
+        <Box ml={10} mt={10} sx={{backgroundColor: 'rgba(52, 52, 52, alpha)',flexGrow: 1 }}>
+
+          <Grid container  alignItems="center" justifyContent="center" spacing={4}>
+              {CardCreator()}
+          </Grid>
+        </Box>
+    </Stack>
 
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,12 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import { WidgetsSharp } from '@mui/icons-material';
 
 export default function ImgMediaCard(props) {
   return (
-    <Card sx={{marginLeft:"10px",marginRight:"10px", maxWidth: 345 }}>
+    <Card sx={{height:"400px",marginLeft:"10px",marginRight:"10px", marginTop:"20px",maxWidth: 345 }}>
       <CardMedia
         component="img"
         alt="green iguana"
@@ -27,8 +29,9 @@ export default function ImgMediaCard(props) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button href={props.link} size="small">Visit</Button>
-        
+        <Stack sx={{width:"100%"}} alignItems="center">
+          <Button href={props.link} size="large" color="secondary" variant="outlined">Visit</Button>
+        </Stack>
       </CardActions>
     </Card>
   );
